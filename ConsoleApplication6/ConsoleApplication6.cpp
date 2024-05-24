43d5fc94a9932efb8282021603f090d0d77d998d
ttw1St0fF4te <isip_e.e.novikov@mpt.ru>
2024-05-24 23:15:17 +0300

    2

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -1,116 +1,112 @@
-﻿#define WIN32_LEAN_AND_MEAN
+﻿#define WIN32_LEAN_AND_MEAN // Эта директива используется для исключения редко используемых сервисов из заголовков Windows, что делает их компиляцию более быстрой.
 
-#include <iostream>
-#include <Windows.h>
-#include <WinSock2.h>
-#include <WS2tcpip.h>
-using namespace std;
-int main()
-{
-	WSADATA wsaData;
-	ADDRINFO* addResult;
-	ADDRINFO hints;
-	SOCKET ConnectSocket = INVALID_SOCKET;
-	const char* sendBuffer = "Hello from Client";
-	const char* sendBuffer2 = "Hello from client 2";
-	char recvBuffer[512];
+#include <iostream> // Включает стандартную библиотеку ввода/вывода.
+#include <Windows.h> // Включает API Windows.
+#include <WinSock2.h> // Включает библиотеку Winsock (версия 2), которая используется для сетевого программирования.
+#include <WS2tcpip.h> // Включает функции TCP/IP библиотеки Winsock.
 
+using namespace std; // Использует стандартное пространство имен.
 
-	int result = WSAStartup(MAKEWORD(2,2), &wsaData);
+int main() {
+    WSADATA wsaData; // Структура, которая используется функцией WSAStartup для получения сведений об реализации Windows Sockets.
+    ADDRINFO* addResult; // Указатель на связанный список структур ADDRINFO, который содержит ответные сведения от вызова функции getaddrinfo.
+    ADDRINFO hints; // Структура, которая используется функцией getaddrinfo для задания критериев запроса.
+    SOCKET ConnectSocket = INVALID_SOCKET; // Дескриптор сокета, который используется для установления соединения с сервером. Он инициализирован как INVALID_SOCKET, чтобы указать, что он в настоящее время не ассоциирован с сокетом.
 
-	if (result != 0) {
-		std::cout << "WSAStartup failes result" << std::endl;
-		return 1;
-	}
+    const char* sendBuffer = "Hello from Client"; // Буфер, который содержит сообщение, которое будет отправлено на сервер.
+    const char* sendBuffer2 = "Hello from client 2"; // Буфер, который содержит второе сообщение, которое будет отправлено на сервер.
+    char recvBuffer[512]; // Буфер, который используется для получения сообщений от сервера.
 
-	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
+    int result = WSAStartup(MAKEWORD(2,2), &wsaData); // Инициализирует библиотеку Winsock. Функция возвращает значение, которое указывает, была ли инициализация успешной.
 
-	result = getaddrinfo("localhost", "666", &hints,&addResult);
-	if (result != 0) {
-		std::cout << "Getaddrinfo wit error" << std::endl;
-		WSACleanup();
-		return 1;
-	}
+    if (result != 0) { // Если инициализация не была успешной, вывести сообщение об ошибке и вернуть 1.
+        std::cout << "WSAStartup failes result" << std::endl;
+        return 1;
+    }
 
-	ConnectSocket = socket(addResult->ai_family, addResult->ai_socktype, addResult->ai_protocol);
-	if (ConnectSocket == INVALID_SOCKET) {
-		std::cout << "Socket creation with " << std::endl;
-		WSACleanup();
-		return 1;
-	}
+    ZeroMemory(&hints, sizeof(hints)); // Инициализирует структуру hints нулем.
+    hints.ai_family = AF_INET; // Указывает, что запрос должен возвращать IPv4-адреса.
+    hints.ai_socktype = SOCK_STREAM; // Указывает, что запрос должен возвращать потоковые сокеты (т.е. TCP-сокеты).
+    hints.ai_protocol = IPPROTO_TCP; // Указывает, что запрос должен возвращать TCP-сокеты.
 
-	result = connect(ConnectSocket, addResult->ai_addr, (int)addResult->ai_addrlen);
-	if (result == SOCKET_ERROR) {
-		std::cout << "Unable to connect to server" << std::endl;
-		closesocket(ConnectSocket);
-		freeaddrinfo(addResult);
-		WSACleanup();
-		return 1;
-	}
+    result = getaddrinfo("localhost", "666", &hints,&addResult); // Запрашивает у DNS IP-адрес и номер порта сервера. Функция возвращает значение, которое указывает, был ли запрос успешным.
+    if (result != 0) { // Если запрос не был успешным, вывести сообщение об ошибке, очистить библиотеку Winsock и вернуть 1.
+        std::cout << "Getaddrinfo wit error" << std::endl;
+        WSACleanup();
+        return 1;
+    }
 
-	result = send(ConnectSocket, sendBuffer, (int)strlen(sendBuffer), 0);
-	if (result == SOCKET_ERROR) {
-		cout << "Send failed, error: " << result << endl;
-		closesocket(ConnectSocket);
-		freeaddrinfo(addResult);
-		WSACleanup();
-		return 1;
-	}
+    ConnectSocket = socket(addResult->ai_family, addResult->ai_socktype, addResult->ai_protocol); // Создает сокет. Функция возвращает дескриптор сокета, который используется для идентификации сокета.
+    if (ConnectSocket == INVALID_SOCKET) { // Если создание сокета не было успешным, вывести сообщение об ошибке, очистить библиотеку Winsock и вернуть 1.
+        std::cout << "Socket creation with " << std::endl;
+        WSACleanup();
+        return 1;
+    }
 
-	std::cout << "Data sent succsesfully" << result << std::endl;
+    result = connect(ConnectSocket, addResult->ai_addr, (int)addResult->ai_addrlen); // Устанавливает соединение с сервером. Функция возвращает значение, которое указывает, было ли соединение успешным.
+    if (result == SOCKET_ERROR) { // Если соединение не было успешным, вывести сообщение об ошибке, закрыть сокет, очистить память и библиотеку Winsock, и вернуть 1.
+        std::cout << "Unable to connect to server" << std::endl;
+        closesocket(ConnectSocket);
+        freeaddrinfo(addResult);
+        WSACleanup();
+        return 1;
+    }
 
-	result = send(ConnectSocket, sendBuffer2, (int)strlen(sendBuffer2), 0);
-	if (result == SOCKET_ERROR) {
-		cout << "Send failed, error: " << result << endl;
-		closesocket(ConnectSocket);
-		freeaddrinfo(addResult);
-		WSACleanup();
-		return 1;
-	}
+    result = send(ConnectSocket, sendBuffer, (int)strlen(sendBuffer), 0); // Отправляет сообщение на сервер. Функция возвращает значение, которое указывает, была ли отправка успешной.
+    if (result == SOCKET_ERROR) { // Если отправка не была успешной, вывести сообщение об ошибке, закрыть сокет, очистить память и библиотеку Winsock, и вернуть 1.
+        cout << "Send failed, error: " << result << endl;
+        closesocket(ConnectSocket);
+        freeaddrinfo(addResult);
+        WSACleanup();
+        return 1;
+    }
 
-	std::cout << "Data sent succsesfully" <<result<< std::endl;
-	
-	result = shutdown(ConnectSocket, SD_SEND);
-	if (result == SOCKET_ERROR) {
-		cout << "shutdown error <3" << endl;
-		freeaddrinfo(addResult);
-		WSACleanup();//очситка WSAStartup
-		return 1;
-	}
+    std::cout << "Data sent succsesfully" << result << std::endl; // Вывести сообщение об успешной отправке и количестве отправленных байтов.
 
-	do {
-		ZeroMemory(recvBuffer, 512);
-		result = recv(ConnectSocket, recvBuffer, 512, 0);
-		if (result > 0) {
-			cout << "Received " << result << "bytes" << endl;
-			cout << "Received data " << recvBuffer << endl;
+    result = send(ConnectSocket, sendBuffer2, (int)strlen(sendBuffer2), 0); // Отправляет второе сообщение на сервер.
+    if (result == SOCKET_ERROR) { // Если отправка не была успешной, вывести сообщение об ошибке, закрыть сокет, очистить память и библиотеку Winsock, и вернуть 1.
+        cout << "Send failed, error: " << result << endl;
+        closesocket(ConnectSocket);
+        freeaddrinfo(addResult);
+        WSACleanup();
+        return 1;
+    }
 
-		}
-		else if (result == 0)
-			cout << "Connection closed" << endl;
+    std::cout << "Data sent succsesfully" <<result<< std::endl; // Вывести сообщение об успешной отправке и количестве отправленных байтов.
 
-		
-	else {
-		cout << "Received failed " << endl;
-		closesocket(ConnectSocket);
+    result = shutdown(ConnectSocket, SD_SEND); // Закрывает отправляющую часть соединения. Функция возвращает значение, которое указывает, было ли закрытие успешным.
+    if (result == SOCKET_ERROR) { // Если закрытие не было успешным, вывести сообщение об ошибке, очистить память и библиотеку Winsock, и вернуть 1.
+        cout << "shutdown error <3" << endl;
+        freeaddrinfo(addResult);
+        WSACleanup();//очситка WSAStartup
+        return 1;
+    }
 
-	}
-	} while (result > 0);
-	result = shutdown(ConnectSocket, SD_SEND);
-	if (result == SOCKET_ERROR) {
-		cout << "shutdown  error " << endl;
-		freeaddrinfo(addResult);
-		WSACleanup();
-		return 1;
-	}
-	closesocket(ConnectSocket);
-	freeaddrinfo(addResult);
-	WSACleanup();
-	return 0;
+    do { // Получает сообщения от сервера, пока соединение не будет закрыто.
+        ZeroMemory(recvBuffer, 512); // Инициализирует буфер получения нулем.
+        result = recv(ConnectSocket, recvBuffer, 512, 0); // Получает сообщение от сервера. Функция возвращает значение, которое указывает, была ли получение успешной.
+        if (result > 0) { // Если получение было успешным, вывести количество полученных байтов и полученные данные.
+            cout << "Received " << result << "bytes" << endl;
+            cout << "Received data " << recvBuffer << endl;
+        }
+        else if (result == 0) // Если соединение было закрыто, вывести сообщение.
+            cout << "Connection closed" << endl;
+        else { // Если получение не было успешным, вывести сообщение об ошибке, закрыть сокет и выйти из цикла.
+            cout << "Received failed " << endl;
+            closesocket(ConnectSocket);
+        }
+    } while (result > 0);
 
+    result = shutdown(ConnectSocket, SD_SEND); // Закрывает отправляющую часть соединения еще раз. Это необязательно, но это не причиняет вреда.
+    if (result == SOCKET_ERROR) { // Если закрытие не было успешным, вывести сообщение об ошибке, очистить память и библиотеку Winsock, и вернуть 1.
+        cout << "shutdown  error " << endl;
+        freeaddrinfo(addResult);
+        WSACleanup();
+        return 1;
+    }
 
+    closesocket(ConnectSocket); // Закрывает сокет.
+    freeaddrinfo(addResult); // Очищает память.
+    WSACleanup(); // Очищает библиотеку Winsock.
+    return 0; // Возвращает 0, чтобы указать, что программа выполнена успешно.
 }
-
--- ConsoleApplication7/ConsoleApplication7.cpp
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -1,113 +1,115 @@
-﻿#define WIN32_LEAN_AND_MEAN
+﻿#define WIN32_LEAN_AND_MEAN // Эта директива исключает из заголовков Windows неиспользуемые компоненты, ускоряя тем самым компиляцию.
 
-#include <iostream>
-#include <Windows.h>
-#include <WinSock2.h>
-#include <WS2tcpip.h>
+#include <iostream> // Подключает стандартную библиотеку ввода-вывода.
+#include <Windows.h> // Подключает базовые компоненты Windows API.
+#include <WinSock2.h> // Подключает библиотеку Winsock для работы с сетевыми сокетами.
+#include <WS2tcpip.h> // Подключает дополнительные компоненты Winsock для работы с протоколом TCP/IP.
 
-int main()
-{
-	WSADATA wsaData;
-	ADDRINFO* addResult;
-	ADDRINFO hints;
-	SOCKET ClientSocket = INVALID_SOCKET;
-	SOCKET ListentSocket = INVALID_SOCKET;
+int main() {
+    WSADATA wsaData; // Структура для хранения информации о реализации сокетов в операционной системе.
+    ADDRINFO* addResult; // Указатель на список структур ADDRINFO, содержащих информацию об адресе сервера.
+    ADDRINFO hints; // Структура, содержащая настройки для функции getaddrinfo().
+    SOCKET ClientSocket = INVALID_SOCKET; // Сокет для подключения клиентов. Инициализирован как INVALID_SOCKET для проверки на корректность.
+    SOCKET ListentSocket = INVALID_SOCKET; // Сокет для прослушивания входящих подключений. Инициализирован как INVALID_SOCKET для проверки на корректность.
 
-	const char* sendBuffer = "Hello from server";
-	char recvBuffer[512];
+    const char* sendBuffer = "Hello from server"; // Буфер для отправки сообщения клиенту.
+    char recvBuffer[512]; // Буфер для получения сообщения от клиента.
 
-	int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    int result = WSAStartup(MAKEWORD(2, 2), &wsaData); // Инициализация библиотеки Winsock. Возвращает 0 в случае успеха.
 
-	if (result != 0) {
-		std::cout << "WSAStartup failes result" << std::endl;
-		return 1;
-	}
+    if (result != 0) { // Если инициализация не удалась, выводится сообщение об ошибке и возвращается код 1.
+        std::cout << "WSAStartup failes result" << std::endl;
+        return 1;
+    }
 
-	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-	hints.ai_flags = AI_PASSIVE;
+    ZeroMemory(&hints, sizeof(hints)); // Обнуление памяти, выделенной под структуру hints.
+    hints.ai_family = AF_INET; // Установка семейства адресов для IPv4.
+    hints.ai_socktype = SOCK_STREAM; // Установка типа сокета для потоковой передачи данных.
+    hints.ai_protocol = IPPROTO_TCP; // Установка протокола TCP для передачи данных.
+    hints.ai_flags = AI_PASSIVE; // Установка флага AI_PASSIVE для использования адреса сервера.
 
-	result = getaddrinfo(NULL, "666", &hints, &addResult);
-	if (result != 0) {
-		std::cout << "Getaddrinfo wit error" << std::endl;
-		WSACleanup();
-		return 1;
-	}
+    result = getaddrinfo(NULL, "666", &hints, &addResult); // Получение информации об адресе сервера с помощью функции getaddrinfo(). Возвращает 0 в случае успеха.
+    if (result != 0) { // Если получение информации не удалось, выводится сообщение об ошибке, выполняется очистка библиотеки Winsock с помощью функции WSACleanup() и возвращается код 1.
+        std::cout << "Getaddrinfo wit error" << std::endl;
+        WSACleanup();
+        return 1;
+    }
 
-	ListentSocket = socket(addResult->ai_family, addResult->ai_socktype, addResult->ai_protocol);
-	if (ListentSocket == INVALID_SOCKET) {
-		std::cout << "Socket creation with " << std::endl;
-		freeaddrinfo(addResult);
-		return 1;
-	}
+    ListentSocket = socket(addResult->ai_family, addResult->ai_socktype, addResult->ai_protocol); // Создание сокета для прослушивания входящих подключений с помощью функции socket(). Возвращает дескриптор сокета в случае успеха.
+    if (ListentSocket == INVALID_SOCKET) { // Если создание сокета не удалось, выводится сообщение об ошибке, выполняется очистка памяти, выделенной под список структур ADDRINFO с помощью функции freeaddrinfo(), и возвращается код 1.
+        std::cout << "Socket creation with " << std::endl;
+        freeaddrinfo(addResult);
+        return 1;
+    }
 
-	result = bind(ListentSocket, addResult->ai_addr, (int)addResult->ai_addrlen);
-	if (result == SOCKET_ERROR) {
-		std::cout << "Binding connect failed" << std::endl;
-		closesocket(ListentSocket);
-		freeaddrinfo(addResult);
-		WSACleanup();
-		return 1;
-	}
+    result = bind(ListentSocket, addResult->ai_addr, (int)addResult->ai_addrlen); // Привязка сокета к адресу сервера с помощью функции bind(). Возвращает 0 в случае успеха.
+    if (result == SOCKET_ERROR) { // Если привязка не удалась, выводится сообщение об ошибке, выполняется закрытие сокета с помощью функции closesocket(), очистка памяти и библиотеки Winsock и возвращается код 1.
+        std::cout << "Binding connect failed" << std::endl;
+        closesocket(ListentSocket);
+        freeaddrinfo(addResult);
+        WSACleanup();
+        return 1;
+    }
 
-	result = listen(ListentSocket, SOMAXCONN);
-	if (result == SOCKET_ERROR) {
-		std::cout << "Listening socket failed" << std::endl;
-		closesocket(ListentSocket);
-		freeaddrinfo(addResult);
-		WSACleanup();
-		return 1;
-	}
+    result = listen(ListentSocket, SOMAXCONN); // Перевод сокета в режим прослушивания входящих подключений с помощью функции listen(). Возвращает 0 в случае успеха.
+    if (result == SOCKET_ERROR) { // Если перевод не удался, выводится сообщение об ошибке, выполняется закрытие сокета, очистка памяти и библиотеки Winsock и возвращается код 1.
+        std::cout << "Listening socket failed" << std::endl;
+        closesocket(ListentSocket);
+        freeaddrinfo(addResult);
+        WSACleanup();
+        return 1;
+    }
 
-	ClientSocket = accept(ListentSocket, NULL, NULL);
-	if (ClientSocket == INVALID_SOCKET) {
-		std::cout << "Accepting socket failed" << std::endl;
-		closesocket(ListentSocket);
-		freeaddrinfo(addResult);
-		WSACleanup();
-		return 1;
-	}
+    ClientSocket = accept(ListentSocket, NULL, NULL); // Ожидание входящего подключения и получение дескриптора сокета клиента с помощью функции accept(). Возвращает дескриптор сокета в случае успеха.
+    if (ClientSocket == INVALID_SOCKET) { // Если получение дескриптора не удалось, выводится сообщение об ошибке, выполняется закрытие сокета, очистка памяти и библиотеки Winsock и возвращается код 1.
+        std::cout << "Accepting socket failed" << std::endl;
+        closesocket(ListentSocket);
+        freeaddrinfo(addResult);
+        WSACleanup();
+        return 1;
+    }
 
-	closesocket(ListentSocket);
-	do {
-		ZeroMemory(recvBuffer, 512);
-		result = recv(ClientSocket, recvBuffer, 512, 0);
-		if (result > 0) {
-			std::cout << "Recieved " << result << "bytes" << std::endl;
-			std::cout << "Recieved data" << recvBuffer << std::endl;
+    closesocket(ListentSocket); // Закрытие сокета для прослушивания входящих подключений.
 
-			result = send(ClientSocket, sendBuffer, (int)strlen(sendBuffer), 0);
+    do { // Цикл обработки сообщений от клиента.
+        ZeroMemory(recvBuffer, 512); // Обнуление памяти, выделенной под буфер для получения сообщения от клиента.
+        result = recv(ClientSocket, recvBuffer, 512, 0); // Получение сообщения от клиента с помощью функции recv(). Возвращает количество полученных байтов в случае успеха.
+        if (result > 0) { // Если сообщение получено, выводится информация о количестве байтов и самом сообщении, а затем отправляется ответное сообщение с помощью функции send().
+            std::cout << "Recieved " << result << "bytes" << std::endl;
+            std::cout << "Recieved data" << recvBuffer << std::endl;
 
-			if (result == SOCKET_ERROR) {
-				std::cout << "Failed to send data back" << std::endl;
-				closesocket(ClientSocket);
-				freeaddrinfo(addResult);
-				WSACleanup();
-			}
-		}
-		else if (result == 0)
-			std::cout << "Connection closing..." << std::endl;
+            result = send(ClientSocket, sendBuffer, (int)strlen(sendBuffer), 0);
 
-		else {
-			std::cout << "recv failed with error" << std::endl;
-			closesocket(ClientSocket);
-			freeaddrinfo(addResult);
-			WSACleanup();
-			return 1;
-		}
-	} while (result > 0);
+            if (result == SOCKET_ERROR) { // Если отправка сообщения не удалась, выводится сообщение об ошибке, выполняется закрытие сокета, очистка памяти и библиотеки Winsock и возвращается код 1.
+                std::cout << "Failed to send data back" << std::endl;
+                closesocket(ClientSocket);
+                freeaddrinfo(addResult);
+                WSACleanup();
+            }
+        }
+        else if (result == 0) // Если клиент отключился, выводится сообщение об этом.
+            std::cout << "Connection closing..." << std::endl;
 
-	result = shutdown(ClientSocket, SD_SEND);
-	if (result == SOCKET_ERROR) {
-		closesocket(ClientSocket);
-		freeaddrinfo(addResult);
-		WSACleanup();
-		return 1;
-	}
-	closesocket(ClientSocket);
-	freeaddrinfo(addResult);
-	WSACleanup();
-	return 0;
+        else { // Если произошла ошибка при получении сообщения, выводится сообщение об ошибке, выполняется закрытие сокета, очистка памяти и библиотеки Winsock и возвращается код 1.
+            std::cout << "recv failed with error" << std::endl;
+            closesocket(ClientSocket);
+            freeaddrinfo(addResult);
+            WSACleanup();
+            return 1;
+        }
+    } while (result > 0); // Цикл продолжается, пока не будет получено сообщение с количеством байтов, равным 0 (т.е. пока клиент не отключится).
+
+    result = shutdown(ClientSocket, SD_SEND); // Завершение работы сокета клиента с помощью функции shutdown(). Возвращает 0 в случае успеха.
+    if (result == SOCKET_ERROR) { // Если завершение не удалось, выводится сообщение об ошибке, выполняется закрытие сокета, очистка памяти и библиотеки Winsock и возвращается код 1.
+        closesocket(ClientSocket);
+        freeaddrinfo(addResult);
+        WSACleanup();
+        return 1;
+    }
+
+    closesocket(ClientSocket); // Закрытие сокета клиента.
+    freeaddrinfo(addResult); // Очистка памяти, выделенной под список структур ADDRINFO.
+    WSACleanup(); // Очистка библиотеки Winsock.
+
+    return 0; // Возврат кода 0, обозначающего успешное завершение работы программы.
 }
